f65a0ed59a604b855ca8894b3942a02eda1ff123
Kshitij Jain <kshitijjain91@gmail.com>
2017-01-04 10:57:25 +0530

    initial commit

---
+++ tournament.sql
@@ -0,0 +1,30 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-------------------------------defining tournament database schema---------------
+
+-- connect to the tournament database
+\c tournament;
+
+-- create players table
+drop table if exists players;
+create table players (player_id serial,
+player_name text);
+
+
+-- create matches table
+drop table if exists matches;
+create table matches (match_id serial,
+player1_id int,
+player2_id int,
+winner_id int,
+loser_id int);
+
+
+
+
